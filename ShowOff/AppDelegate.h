a18d71ade7bb0153dfd8d6402f5193f9501eb5f0
mesird <holy_mesird@126.com>
2015-06-16 01:19:51 +0800

    Project setup

---
+++ ShowOff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowOff
+//
+//  Created by mesird on 6/16/15.
+//  Copyright (c) 2015 mesird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
